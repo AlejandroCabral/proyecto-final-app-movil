e37e10322519f2b25c74a1b98c475c264b08f0d5
bryan-peguero <medranobryan22@gmail.com>
2022-04-19 20:35:36 -0400

    Login creado / Iniciar pagina de bienvenida

---
+++ src/components/Login/LoginForm.tsx
@@ -0,0 +1,41 @@
+import './LoginForm.css';
+import { IonIcon } from '@ionic/react';
+import { personOutline, key } from 'ionicons/icons';
+
+interface LoginProps { }
+
+const LoginForm: React.FC<LoginProps> = () => {
+  return (
+    <div className="container fade">
+
+      <img className="header-image" src="https://coopdgii.com/wp-content/uploads/2016/05/Logo-300x300.png" alt="Logo de la company" />
+      
+      <div className="login-form">
+
+        <div className="cedula">
+          <input type="text" placeholder="Cédula"/><IonIcon icon={personOutline}></IonIcon>
+        </div>
+
+          <div className="password">
+          <input type="text" placeholder="Contraseña"/><IonIcon icon={key}></IonIcon>
+          </div>
+
+        <div className="boton">
+          <button type="submit">INICIAR SESIÓN</button>
+        </div>
+
+        <div className="error">
+          <a href="#O">¿Tienes problemas para Iniciar Sesión?</a>
+        </div>
+
+      </div>
+
+      <footer>
+        {/* <img src="https://i1.wp.com/www.revistamercado.do/wp-content/uploads/2021/05/mujer-contadora.jpg?fit=1280%2C638&ssl=1" alt="Logo mujer emprendedora" /> */}
+      </footer>
+    
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage } from '@ionic/react';
 import LoginForm from '../components/Login/LoginForm';
 import './Home.css';
 
@@ -6,16 +6,8 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent fullscreen scroll-y="false">
         <LoginForm />
 
       {/* TODO: AGREGAR PAGINA INICIAL/BIENVENIDA CON MENU DESPLEGABLE PARA ACCEDER A LAS CUENTAS, NOTICIAS, ETC... E ICONO DE NOTIFICACIONES  */}
